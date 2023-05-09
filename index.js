13759fb858141f07e2fe2fa1f2b7038f6ad6023b
eat-a-p3ach <lyncyyhodge@gmail.com>
2023-05-09 18:10:53 -0500

    trying to fix nav bar

--- index.js
+++ index.js
@@ -30,31 +30,32 @@ router.hooks({
       params && params.data && params.data.view
         ? capitalize(params.data.view)
         : "Home";
+    // Add a switch case statement to handle multiple routes
     switch (view) {
-        case "Home":
-          axios
-              .get(
-              `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
-            )
-            .then(response => {
-              // Convert Kelvin to Fahrenheit since OpenWeatherMap does provide otherwise
-              const kelvinToFahrenheit = kelvinTemp =>
-                Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
-
-  store.Home.weather = {
-    city: response.data.name,
-    temp: kelvinToFahrenheit(response.data.main.temp),
-    feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
-    description: response.data.weather[0].main
-  };
-  done();
-})
-.catch(err => {
-  console.log(err);
-  done();
-});
-break;
-
+      case "Home":
+        axios
+          // Get request to retrieve the current weather data using the API key and providing a city name
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+          )
+          .then(response => {
+            // Convert Kelvin to Fahrenheit since OpenWeatherMap does provide otherwise
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+            // Create an object to be stored in the Home state from the response
+            store.Home.weather = {
+              city: response.data.name,
+              temp: kelvinToFahrenheit(response.data.main.temp),
+              feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
+              description: response.data.weather[0].main
+            };
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+        break;
       case "Mytraining":
         axios
           .get(
@@ -65,8 +66,11 @@ break;
             done(); // Added done() here
           });
         break;
-
-already: params => {
+      default:
+        done();
+    }
+  },
+  already: params => {
     const view =
       params && params.data && params.data.view
         ? capitalize(params.data.view)
@@ -75,7 +79,6 @@ already: params => {
     render(store[view]);
   }
 });
-
 router
   .on({
     "/": () => render(),
