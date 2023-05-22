8f1a680f185c7de4a1730223d0d9cd3ae7929a54
Lyncy Hodge <lyncyyhodge@gmail.com>
2023-05-21 22:09:13 -0500

    working on express routes

--- components/views/MyTraining.js
+++ components/views/MyTraining.js
@@ -7,4 +7,38 @@ export default state =>
     </section>
     <br />
     <div id="calendar"></div>
+
+    <div class="form">
+      <form action="" method="POST" class="library">
+        <label for="name">Enter Your Name:</label>
+        <input type="text" name="name" id="name" placeholder="Your Name" />
+
+        <label for="date">DATE</label>
+        <input type="text" name="date" id="date" placeholder="Today's date" />
+
+        <label for="tag">tag:</label>
+        <select name="tag" id="tag">
+          <option value="tag">#</option>
+          <option value="tag"></option>
+        </select>
+
+        <label for="move">Tag this new move</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="for example: armbar"
+        />
+
+        <div>
+          <label for="msg">Enter your message:</label>
+          <textarea name="msg" id="msg" cols="50" rows="20"></textarea>
+        </div>
+
+        <div>
+          <label for="enter">Save to my library</label>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </div>
   `;
