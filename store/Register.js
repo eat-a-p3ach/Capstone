655b89beab6e556fda264b2845306a1e1573a0f8
Lyncy Hodge <lyncyyhodge@gmail.com>
2023-05-22 20:58:16 -0500

    week 10

--- store/Register.js
+++
@@ -1,4 +0,0 @@
-export default {
-  header: "Register",
-  view: "Register"
-};
--- store/index.js
+++ store/index.js
@@ -2,6 +2,5 @@ export { default as About } from "./About";
 export { default as Home } from "./Home";
 export { default as Contact } from "./Contact";
 export { default as Links } from "./Links";
-export { default as Mytraining } from "./MyTraining";
-// export { default as Login } from "./Login";
-// export { default as Register } from "./Register";
+export { default as Mytraining } from "./Mytraining";
+export { default as Mymoves } from "./Mymoves";
