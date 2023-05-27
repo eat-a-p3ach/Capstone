acfef514f23b33810250c5b35ddf6705aa08f7d6
Lyncy Hodge <lyncyyhodge@gmail.com>
2023-05-27 10:05:53 -0500

    working on fullcalendario

--- index.js
+++ index.js
@@ -18,12 +18,174 @@ function render(state = store.Home) {
   afterRender(state);
 
   router.updatePageLinks();
+
+  addEventListeners(state);
 }
+
+function handleEventDragResize(info) {
+  const event = info.event;
+
 // add menu toggle to bars icon in nav bar
 function afterRender(state) {
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
+
+
+  if (confirm("Are you sure about this change?")) {
+    const requestData = {
+      customer: event.title,
+      start: event.start.toJSON(),
+      end: event.end.toJSON(),
+      url: event.url
+    };
+
+
+    axios
+      .put(`${process.env.API_URL}/appointments/${event.id}`, requestData)
+      .then(response => {
+        console.log(`Event '${response.data.customer}' (${response.data._id}) has been updated.`);
+      })
+      .catch(error => {
+        info.revert();
+        console.log("It puked", error);
+      });
+  } else {
+    info.revert();
+  }
+}
+
+
+function addEventListeners(state) {
+  // add menu toggle to bars icon in nav bar
+  document
+    .querySelector(".fa-bars")
+    .addEventListener("click", () =>
+      document.querySelector("nav > ul").classList.toggle("hidden--mobile")
+    );
+
+
+  if (state.view === "Schedule") {
+      document.querySelector("form").addEventListener("submit", event => {
+        event.preventDefault();
+
+        const inputList = event.target.elements;
+
+        //this part
+        const requestData = {
+          customer: inputList.customer.value,
+          start: new Date(inputList.start.value).toJSON(),
+          end: new Date(inputList.end.value).toJSON(),
+        };
+
+        //what url do I use here?
+        axios
+          .post(`${process.env.API_URL}/appointments`, requestData)
+          .then(response => {
+            // Push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+            store.Appointments.appointments.push(response.data);
+            router.navigate("/appointments");
+          })
+          .catch(error => {
+            console.log("It puked", error);
+          });
+      });
+    }
+
+//lines 83-171 in https://github.com/savvy-coders/full-calendar-spa-example/blob/master/index.js
+
+if (state.view === "Appointments" && state.appointments) {
+  const calendarEl = document.getElementById('calendar');
+  calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    buttonText: {
+      today:    'Today',
+      month:    'Month',
+      week:     'Week',
+      day:      'Day',
+      list:     'List'
+    },
+    height: '100%',
+    dayMaxEventRows: true,
+    navLinks: true,
+    editable: true,
+    selectable: true,
+    eventClick: function(info) {
+      // change the border color just for fun
+      info.el.style.borderColor = 'red';
+    },
+    eventDrop: function(info) {
+      handleEventDragResize(info);
+    },
+    eventResize: function(info) {
+      handleEventDragResize(info);
+    },
+    select: (info) => {
+      const customer = prompt("Please enter a title");
+
+      if (customer) {
+        const requestData = {
+          customer: customer,
+          start: info.start.toJSON(),
+          end: info.end.toJSON(),
+          allDay: info.view.type === 'dayGridMonth'
+        };
+
+        axios
+        .post(`${process.env.API_URL}/appointments`, requestData)
+        .then(response => {
+          // Push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          response.data.title = response.data.customer;
+          response.data.url = `/appointments/${response.data._id}`;
+          console.log('lyncy-response.data:', response.data);
+          store.Appointments.appointments.push(response.data);
+          console.log(`Event '${response.data.customer}' (${response.data._id}) has been created.`);
+          calendar.addEvent(response.data);
+          calendar.unselect();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+      } else {
+        calendar.unselect();
+      }
+    },
+    events: state.appointments || []
+  });
+  calendar.render();
+}
+
+if (state.view === "Appointments" && state.event) {
+  const deleteButton = document.getElementById("delete-appointment");
+  deleteButton.addEventListener("click", (event) => {
+    deleteButton.disabled = true;
+    console.log('lyncy-event.target.dataset.id:', event.target.dataset.id);
+
+    if (confirm("Are you sure you want to delete this appointment")) {
+      axios
+      .delete(`${process.env.API_URL}/appointments/${event.target.dataset.id}`)
+      .then(response => {
+        // Push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+        console.log(`Event '${response.data.customer}' (${response.data._id}) has been deleted.`);
+        router.navigate('/appointments');
+      })
+      .catch(error => {
+        console.log("It puked", error);
+      });
+    } else {
+      deleteButton.disabled = false;
+    }
+  });
+}
+}
+
+// end lines 83-171
+
 
   if (state.view === "Mytraining") {
     document.querySelector("form").addEventListener("submit", event => {
@@ -80,6 +242,8 @@ function handleEventDragResize(info) {
       text: event.text
     };
 
+
+
 router.hooks({
   before: (done, params) => {
     const view =
@@ -131,6 +295,13 @@ router.hooks({
         done();
     }
   },
+
+  // line 173 https://github.com/savvy-coders/full-calendar-spa-example/blob/master/index.js
+  // convert these if statements to switch cases
+  case ""
+
+
+
   already: params => {
     const view =
       params && params.data && params.data.view
@@ -140,6 +311,13 @@ router.hooks({
     // render(store[view]);
   }
 });
+
+
+
+
+
+
+
 router
   .on({
     "/": () => render(),
